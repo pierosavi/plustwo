bdc5da64a7c717339d7269688292eb72cc2ce968
pierosavi <15440183+pierosavi@users.noreply.github.com>
2021-07-08 21:33:04 +0200

    Add dark theme switch

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
-    <n-layout>
-      <n-layout-content content-style="padding: 24px">
-        <router-view/>
-      </n-layout-content>
-    </n-layout>
+    <n-config-provider v-model:theme="theme">
+      <n-global-style />
+      <n-layout>
+        <n-layout-header bordered style="height: 64px; padding: 24px;">
+            PLUSTWO
+            <n-switch v-model:value="darkMode" />
+        </n-layout-header>
+        <n-layout-content content-style="padding: 24px">
+          <router-view/>
+        </n-layout-content>
+      </n-layout>
+    </n-config-provider>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { NLayout, NLayoutContent } from 'naive-ui'
+import { NLayout, NLayoutContent, NConfigProvider, NGlobalStyle, darkTheme, NLayoutHeader, NSwitch } from 'naive-ui'
 
 export default defineComponent({
   name: 'App',
   components: {
     NLayout,
-    NLayoutContent
+    NLayoutContent,
+    NConfigProvider,
+    NGlobalStyle,
+    NLayoutHeader,
+    NSwitch
+  },
+  data: function () {
+    return {
+      darkMode: true
+    }
+  },
+  computed: {
+    theme: function () {
+      return this.darkMode ? darkTheme : null
+    }
   }
 })
 </script>
